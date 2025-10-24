c225542f3031afd5db034b730e6921a81d3ef62c
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-10-24 20:53:29 +0530

    list method

--- Lists.py
+++ Lists.py
@@ -1,5 +1,23 @@
-marks = [39,86.4,98,56.3]
-print(marks)
-print(type(marks))
-print(marks[2])
-print(marks[len(marks)-1])
+# marks = [39,86.4,98,56.3]
+# print(marks)
+# print(type(marks))
+# print(marks[2])
+# print(marks[len(marks)-1])
+
+#........List are mutable..........
+
+student = ["pihu",89,20,"jabalpur"]
+# print(student)
+# student[0] = "Harshita"
+# print(student)
+# print(student[0:3])
+
+student.append("English")
+print(student)
+list=[56,2,79,2,0]
+#list.sort(reverse=True) #des sort
+# list.reverse()
+# list.insert(2,6)
+list.remove(2)
+list.pop(3)
+print(list)
