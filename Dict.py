724861c70a8b054de12d3d2a3183c1abf8accd44
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-10-24 22:14:47 +0530

    dictionary

---
+++ Dict.py
@@ -0,0 +1,24 @@
+# info = {
+#     "key" : "value",
+#     "name" : "Pihu",
+#     "subj" : ["python","java","html"],
+#     "age" : 20 ,
+#      2 : 75
+# }
+# info["name"] = "Harshita"
+# info["surname"] = "Koshta"
+# print(info)
+# print(info["name"])
+# print(info["subj"])
+# print(info["age"])
+
+
+student = {
+    "name" : "rahul",
+    "subj" : {
+        "english" : 78,
+        "maths" : 97,
+        "chemistry" : 84
+    }
+}
+print(student["subj"]["maths"])
