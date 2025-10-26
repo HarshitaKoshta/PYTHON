490fa0e60c453661aca66df2c6180b9d09e5f5b2
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-10-26 21:13:54 +0530

    square area

---
+++ SquareArea.py
@@ -0,0 +1,10 @@
+theta = 60
+radius = 42
+
+arc_length = (theta / 360)*2*3.14*radius
+side = arc_length/4
+area = side * side
+
+print("Length of the wire arc =",arc_length)
+print("Side of the square =", side )
+print("Area of the square =", area)
