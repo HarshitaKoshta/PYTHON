6d5ac5fab46c541204a5c74fdc134874201619cb
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-10-27 22:08:56 +0530

    inch to other

---
+++ inch.py
@@ -0,0 +1,11 @@
+inches = float(input("Enter measurement in inches to convert : "))
+
+foot = inches/ 12
+yards = inches/36
+cm = inches * 2.54
+meters = inches / 39.37
+
+print("in foot:",foot)
+print("in yard:",yards)
+print("in centimeters:",cm)
+print("in meters:",meters)
