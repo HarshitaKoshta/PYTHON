010e3a9a9de6836f329fd3716163424fc512a586
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-10-29 21:32:39 +0530

    recursion

---
+++ Recursion.py
@@ -0,0 +1,27 @@
+
+# def fact(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     return n*fact(n-1)
+    
+
+# print(fact(5))
+
+
+# def fabo(n):
+#     if n==0:
+#         return 0
+#     if n==1:
+#         return 1
+#     return fabo(n-1) +fabo(n-2)   
+
+# for i in range(10):
+#     print(fabo(i), end=" ")
+
+
+def sum(n):
+    if n==0:
+        return 0
+    return n+sum(n-1)
+
+print(sum(4))    
