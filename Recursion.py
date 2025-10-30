373950585004dd9a28fe094e304177ccd24cac03
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-10-30 15:39:32 +0530

    recursion

--- Recursion.py
+++ Recursion.py
@@ -19,9 +19,18 @@
 #     print(fabo(i), end=" ")
 
 
-def sum(n):
-    if n==0:
-        return 0
-    return n+sum(n-1)
+# def sum(n):
+#     if n==0:
+#         return 0
+#     return n+sum(n-1)
 
-print(sum(4))    
+# print(sum(4))    
+
+
+def print_rev(n):
+    if n==6:
+        return
+    print_rev(n+1)
+    print(n,end=' ')
+
+print_rev(1)
