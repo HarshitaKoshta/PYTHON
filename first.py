25c62f25a7e2cdad9dd7761f103448aba649f626
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-11-05 19:45:47 +0530

    str

--- first.py
+++ first.py
@@ -163,11 +163,16 @@
 #       larg = n      
 # print(larg)    
 
-# check prime
-num = 20
-is_prime = True
-for i in range(2,num):
-    if num%i == 0:
-        is_prime = False
-        break
-print(num,"is prime?",is_prime)    
+# # check prime
+# num = 20
+# is_prime = True
+# for i in range(2,num):
+#     if num%i == 0:
+#         is_prime = False
+#         break
+# print(num,"is prime?",is_prime)    
+
+
+def string(s):
+    return s[::-1]
+print(string('hello'))
