70eda4053d4eef466f91956152ab7938dd829d21
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-11-24 15:17:42 +0530

    tuplr

--- Tuple.py
+++ Tuple.py
@@ -1,13 +1,22 @@
-tup = (3,5,3,6,4)
-t=(4,)
-print(type(tup))
+# tup = (3,5,3,6,4)
+# t=(4,)
+# print(type(tup))
+# print(t)
+# print(tup.count(3))
+
+
+# grade = ("c","d","a","c")
+# print(grade.count("c"))
+
+# grade = ["c","d","a","c"]
+# grade.sort()
+# print(grade)
+
+
+t = (4,5,(3,4),[1,'raju',47,'abs'])
 print(t)
-print(tup.count(3))
 
 
-grade = ("c","d","a","c")
-print(grade.count("c"))
+t = (3,5,7,'pihu')
+print(t)
 
-grade = ["c","d","a","c"]
-grade.sort()
-print(grade)
