e3cb7bf222d180a2dff754f2361f8ba09f0a6d35
HarshitaKoshta <harshitakoshta2111@gmail.com>
2026-01-07 20:52:09 +0530

    range sum

---
+++ Contest/contest2/D.RangeSum.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for i in range(t):
+    L, R = map(int, input().split())
+    
+    if L > R:
+        L,R = R,L     
+    sum_R = R *(R + 1) // 2
+    sum_L = (L - 1)* L // 2
+    print(sum_R - sum_L)
